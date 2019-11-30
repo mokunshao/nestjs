2b31737097c908f4a1bebb42be450f3d0b641110
mokunshao <mokunshao@foxmail.com>
2019-11-30 20:50:30 +0800

    开启新项目

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { LoggingInterceptor } from './core/interceptors/logging.interceptor';
-// import { DemoFilter } from './core/filters/demo.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.useGlobalFilters(new DemoFilter());
-  // app.useGlobalInterceptors(new LoggingInterceptor());
   await app.listen(3000);
 }
 bootstrap();
--- src/modules/posts/posts.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DemoService } from './providers/demo/demo.service'
-import { PostsController } from './posts.controller'
-
-@Module({
-  providers: [DemoService],
-  controllers: [PostsController]
-})
-export class PostsModule { }
