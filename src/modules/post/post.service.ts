843ffccfa7497aa1af9fbfad80089bc56c2b9554
mokunshao <mokunshao@foxmail.com>
2019-12-01 01:39:07 +0800

    注册 Post 实体的 Repository

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Post } from './post.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  constructor(
+    @InjectRepository(Post) private readonly postRepository: Repository<Post>,
+  ) { }
+}
