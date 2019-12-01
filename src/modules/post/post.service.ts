4bcd71990665db8402a3f55bbfc10a95d37489bf
mokunshao <mokunshao@foxmail.com>
2019-12-02 00:46:29 +0800

    查询实体单个数据

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -8,13 +8,20 @@ export class PostService {
   constructor(
     @InjectRepository(Post) private readonly postRepository: Repository<Post>,
   ) { }
+
   async store(data) {
     const entity = await this.postRepository.create(data);
     await this.postRepository.save(entity);
     return entity;
   }
+
   async index() {
     const entities = await this.postRepository.find();
     return entities;
+  }
+
+  async show(id: string) {
+    const entity = await this.postRepository.findOne(id);
+    return entity;
   }
 }
