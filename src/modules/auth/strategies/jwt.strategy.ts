7bf09b7d950cb4522945b89b542a0b325d520d48
mokunshao <mokunshao@foxmail.com>
2019-12-05 00:15:06 +0800

    Update jwt.strategy.ts

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-strategy';
-import { ExtractJwt, VerifiedCallback } from 'passport-jwt';
+import { Strategy, ExtractJwt, VerifiedCallback } from 'passport-jwt';
 import { secretOrPrivateKey } from '../secretOrPrivateKey';
 import { JwtPayload } from '../auth.interface';
 
