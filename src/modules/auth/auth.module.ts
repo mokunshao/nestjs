d4c2ab75e4a1d12b1f6da58e9b2317ae0b31716e
mokunshao <mokunshao@foxmail.com>
2019-12-05 02:22:15 +0800

    在 Post 模块中使用 AuthGuard 验证用户身份

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -22,5 +22,6 @@ import { JwtStrategy } from './strategies/jwt.strategy';
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
+  exports: [PassportModule],
 })
 export class AuthModule { }
