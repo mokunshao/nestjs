9a27879db5875fad40a940bc0b443cadad175863
mokunshao <mokunshao@foxmail.com>
2019-12-04 21:00:31 +0800

    安装与配置 JWT 模块（@nestjs/jwt）

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secretOrPrivateKey: '1575464158091',
+      signOptions: {
+        expiresIn: '12h',
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
