f641dace9fec32f8c10109e6dc6ff6c20648bdb5
mokunshao <mokunshao@foxmail.com>
2019-12-05 22:43:18 +0800

    存储分类实体数据

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Category } from './category.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CategoryDto } from './category.dto';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+  ) { }
+
+  async store(data: CategoryDto) {
+    const entity = await this.categoryRepository.create(data);
+    return await this.categoryRepository.save(entity);
+  }
+}
