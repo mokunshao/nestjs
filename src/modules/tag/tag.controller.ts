b8aa68fa041ff867afcca8aef461bfe0076b3d5a
mokunshao <mokunshao@foxmail.com>
2019-12-06 16:27:27 +0800

    处理标签实体数据

--- src/modules/tag/tag.controller.ts
+++ src/modules/tag/tag.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Put, Param, ParseIntPipe, Delete } from '@nestjs/common';
+import { TagService } from './tag.service';
+import { TagDto } from './tag.dto';
 
-@Controller('tag')
-export class TagController {}
+@Controller('tags')
+export class TagController {
+  constructor(
+    private readonly tagService: TagService,
+  ) { }
+
+  @Post()
+  async store(
+    @Body() data: TagDto,
+  ) {
+    return this.tagService.store(data);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() data: TagDto,
+  ) {
+    return this.tagService.update(id, data);
+  }
+
+  @Delete(':id')
+  async destory(
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return await this.tagService.destory(id);
+  }
+}
