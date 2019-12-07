0b1d910be8d89020a8fdee557d9e14f4d6674ffd
mokunshao <mokunshao@foxmail.com>
2019-12-07 22:08:55 +0800

    在权限控制守卫检查用户角色

--- src/core/guards/access.guard.ts
+++ src/core/guards/access.guard.ts
@@ -1,13 +1,37 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
+import { PermissionInterface } from '../interfaces/permission.interface';
+import { User } from 'src/modules/user/user.entity';
 
 @Injectable()
 export class AccessGuard implements CanActivate {
-  canActivate(
+  constructor(
+    private readonly reflector: Reflector,
+  ) { }
+
+  async validatePermissions(
+    permissions: PermissionInterface[],
+    user: User,
+  ) {
+    const results = permissions.map(async (permission) => {
+      const { role } = permission;
+      let hasRole: boolean;
+
+      if (role) {
+        hasRole = user.roles.some(r => r.name === role);
+      }
+
+      return hasRole;
+    });
+    return Promise.all(results);
+  }
+
+  async canActivate(
     context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  ): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    console.log(request.user)
-    return false;
+    const permissions = this.reflector.get('permissions', context.getHandler());
+    const results = await this.validatePermissions(permissions, request.user);
+    return results.includes(true);
   }
 }
