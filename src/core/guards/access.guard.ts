6a27816ca423d7fbd7f19e362ee0f68950c8f1af
mokunshao <mokunshao@foxmail.com>
2019-12-08 01:08:40 +0800

    用权限控制守卫检查用户是否拥有指定资源

--- src/core/guards/access.guard.ts
+++ src/core/guards/access.guard.ts
@@ -2,26 +2,33 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { PermissionInterface } from '../interfaces/permission.interface';
 import { User } from 'src/modules/user/user.entity';
+import { Possession } from '../enums/possession.enum';
+import { UserService } from 'src/modules/user/user.service';
 
 @Injectable()
 export class AccessGuard implements CanActivate {
   constructor(
     private readonly reflector: Reflector,
+    private readonly userService: UserService,
   ) { }
 
   async validatePermissions(
     permissions: PermissionInterface[],
     user: User,
+    resourceId: number,
   ) {
     const results = permissions.map(async (permission) => {
-      const { role } = permission;
-      let hasRole: boolean;
-
+      const { role, resource, possession } = permission;
+      let hasRole: boolean = true;
+      let hasPossession: boolean = true;
+      if (possession === Possession.OWN) {
+        hasPossession = await this.userService.possess(user.id, resource, resourceId);
+      }
       if (role) {
         hasRole = user.roles.some(r => r.name === role);
       }
 
-      return hasRole;
+      return hasRole && hasPossession;
     });
     return Promise.all(results);
   }
@@ -31,7 +38,7 @@ export class AccessGuard implements CanActivate {
   ): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const permissions = this.reflector.get('permissions', context.getHandler());
-    const results = await this.validatePermissions(permissions, request.user);
+    const results = await this.validatePermissions(permissions, request.user, parseInt(request.params.id, 10));
     return results.includes(true);
   }
 }
